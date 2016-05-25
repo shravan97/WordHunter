259e2162418915320f38ed87d7c711c3f88cfce9
shravan97 <shravanmurali@gmail.com>
2016-05-26 02:47:28 +0530

    unified everything under a single class

--- SearchEngine/google_search.py
+++ SearchEngine/google_search.py
@@ -2,54 +2,68 @@ from googleapiclient.discovery import build
 import pprint , urllib2 ,re
 from bs4 import BeautifulSoup as bs
 
-def search(search_term, api_key, cse_id, **kwargs):    
-    google_service = build("customsearch", "v1", developerKey=api_key)
-    result = google_service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return result['items']
+''' You may notice here that a few functions can be merged and made into a single function .
+But I'm preserving every small function to enhance modularity '''
 
-def get_results(search_term,my_api_key , my_cse_id):
-	urls=[]
-	print "Querying Google ......"
-	for k in range(0,7):
-		res = search(search_term , my_api_key , my_cse_id , start=k*10+1 , num=10)
-		for z in res:
-			if re.match('https' , z['formattedUrl'])!=None:
-				z['formattedUrl']=re.sub('https','http',z['formattedUrl'])
-			elif re.match('http' , z['formattedUrl'])==None:
-				z['formattedUrl'] = 'http://'+z['formattedUrl']
-			urls.append(str(z['formattedUrl']))
-	return urls				
+class GoogleSearch:
 
-def get_words_from_urls(url_list):
-	#Gets all the words from a list of urls
-	words = []
-	for k in url_list:
-		scr = scrape_site(str(k))
-		if scr!=-1:
-			words.append(scr)
-	
-	return words	
+	def __init__(self , api_key , cse_id):
+		self.my_api_key = api_key
+		self.my_cse_id = cse_id
 
-def scrape_site(url):
-	hdr = {'User-Agent': 'Mozilla/5.0'}
-	req = urllib2.Request(url , headers=hdr)
-	try:
-		print 'Scraping '+url+' ......'
-		page = urllib2.urlopen(req)
-		soup = bs(page)
-		[s.extract() for s in soup('script')]
-		[s.extract() for s in soup('style')]
-		phrases=[]
-		words=[]
+	def search(self , search_term, **kwargs):    
+	    google_service = build("customsearch", "v1", developerKey=self.my_api_key)
+	    result = google_service.cse().list(q=search_term, cx=self.my_cse_id, **kwargs).execute()
+	    return result['items']
 
-		for k in soup.strings:
-			phrases.append(re.split('[\n\t ]+' , k))
+	def get_results(self,search_term):
+		urls=[]
+		print "Querying Google ......"
+		for k in range(0,7):
+			res = self.search(search_term , start=k*10+1 , num=10)
+			for z in res:
+				if re.match('https' , z['formattedUrl'])!=None:
+					z['formattedUrl']=re.sub('https','http',z['formattedUrl'])
+				elif re.match('http' , z['formattedUrl'])==None:
+					z['formattedUrl'] = 'http://'+z['formattedUrl']
+				urls.append(str(z['formattedUrl']))
+		return urls
 
-		for k in phrases:
-			for z in k:
-				if z!='':
-					words.append(z)
 
+	@staticmethod	
+	def get_words_from_urls(url_list):
+		#Gets all the words from a list of urls
+		words = []
+		for k in url_list:
+			scr = GoogleSearch.scrape_site(str(k))
+			if scr!=-1:
+				words.append(scr)
+		
 		return words
-	except :
-		return -1
+
+
+	@staticmethod
+	def scrape_site(url):
+		hdr = {'User-Agent': 'Mozilla/5.0'}
+		req = urllib2.Request(url , headers=hdr)
+		try:
+			print 'Scraping '+url+' ......'
+			page = urllib2.urlopen(req)
+			soup = bs(page)
+			[s.extract() for s in soup('script')]
+			[s.extract() for s in soup('style')]
+			phrases=[]
+			words=[]
+	
+			for k in soup.strings:
+				phrases.append(re.split('[\n\t ]+' , k))
+	
+			for k in phrases:
+				for z in k:
+					if z!='':
+						words.append(z)
+	
+			return words
+		except :
+			return -1
+		
