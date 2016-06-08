e0c317d8d1b7998c66c8ca3688bddd1af8183caf
shravan97 <shravanmurali@gmail.com>
2016-06-09 00:22:25 +0530

    rename SearchEngine to Web

--- SearchEngine/__init__.py
+++
@@ -1,1 +0,0 @@
-from google_search import GoogleSearch
--- Web/google_search.py
+++ Web/google_search.py
@@ -5,22 +5,21 @@ from bs4 import BeautifulSoup as bs
 ''' You may notice here that a few functions can be merged and made into a single function .
 But I'm preserving every small function to enhance modularity '''
 
-class GoogleSearch:
+class web:
 
-	def __init__(self , api_key , cse_id):
-		self.my_api_key = api_key
-		self.my_cse_id = cse_id
+	def __init__(self):
+		pass
 
-	def search(self , search_term, **kwargs):    
-	    google_service = build("customsearch", "v1", developerKey=self.my_api_key)
-	    result = google_service.cse().list(q=search_term, cx=self.my_cse_id, **kwargs).execute()
+	def search(self , api_key , cse_id , search_term, **kwargs):    
+	    google_service = build("customsearch", "v1", developerKey=api_key)
+	    result = google_service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
 	    return result['items']
 
-	def get_results(self,search_term):
+	def get_results(self, api_key , cse_id , search_term):
 		urls=[]
 		print "Querying ......"
 		for k in range(0,7):
-			res = self.search(search_term , start=k*10+1 , num=10)
+			res = self.search(api_key , cse_id , search_term , start=k*10+1 , num=10)
 			for z in res:
 				if re.match('https' , z['formattedUrl'])!=None:
 					z['formattedUrl']=re.sub('https','http',z['formattedUrl'])
@@ -36,7 +35,7 @@ class GoogleSearch:
 		words = []
 		print "Scraping and fetching words ......."
 		for k in url_list:
-			scr = GoogleSearch.scrape_site(str(k))
+			scr = web.scrape_site(str(k))
 			if scr!=-1:
 				for word in scr:
 					words.append(word)
@@ -61,7 +60,12 @@ class GoogleSearch:
 	
 			for k in phrases:
 				for z in k:
-					if z!='' and re.match(' [0-9]+ ',z)==None:
+					flag=1
+					for ch in z:
+						if re.match('[a-zA-z]',ch)== None: #Filter out words by checking each character
+							flag=0
+							break
+					if z!='' and flag==1:
 						words.append(z)
 	
 			return words
